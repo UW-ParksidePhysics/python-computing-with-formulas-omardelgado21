b17d89d0885025026a9e1c08648caca9ad2d5f1f
Omar Delgado <odelgadom216@gmail.com>
2021-03-02 21:06:15 +0000

    Finished 1plus1.py

---
+++ seconds2years.py
@@ -0,0 +1,9 @@
+seconds = 1000000000
+minutes = seconds/60
+hours = minutes/60
+days = hours/24
+years = days/365
+
+print("Can a newborn baby expect to live for one billion seconds?")
+print("One billion seconds is equal to", years)
+print("Yes, the baby can expect to live for one billion seconds.")
